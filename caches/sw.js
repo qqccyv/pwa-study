976619788acfb5f524c720c721be97a6de01c2cc
dengyu <dengyu@qq.com>
2022-07-25 15:16:26 +0800

    缓存api调整

--- caches/sw.js
+++ caches/sw.js
@@ -1,5 +1,5 @@
 // 通过更新 CACHE_NAME 版本号来清理旧的缓存
-const CACHE_NAME = "cache_v2"
+const CACHE_NAME = "cache_v3"
 
 // install 周期中主要工作是缓存对应的资源
 self.addEventListener('install', async (event) => {
@@ -7,7 +7,6 @@ self.addEventListener('install', async (event) => {
   try {
     // 开启一个缓存对象 111
     const cache = await caches.open(CACHE_NAME)
-    console.log('开始缓存', cache);
     // 将对应的静态资源缓存
     await cache.addAll([
       './data.json',
@@ -37,9 +36,17 @@ self.addEventListener('activate', async (event) => {
   // clients.claim()  使当前激活的serviceWorker立即生效并控制当前的页面
   await clients.claim()
 })
-self.addEventListener('fetch', (event) => {
-  // 优先网络，如果请求失败，则读取缓存
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)))
+self.addEventListener('fetch', async (event) => {
+  // 优先网络，如果请求失败，则读取缓存 
+  // event.respondWith(fetch(event.request).catch(() => caches.match(event.request)))
+  try {
+    console.log('1111111');
+    event.respondWith(fetch(event.request.url.replace(new URL(event.request.url).hostname, 'localhost')))
+  } catch (error) {
+    console.log('2222222');
+    event.respondWith(caches.match(event.request))
+  }
+
 
 })
 
